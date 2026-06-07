dad7e0d432bbdd78f484f3f9bce3473aad770a1d
Diegoudg94 <j.covarrubias@udgvirtual.udg.mx>
2026-06-07 09:27:02 -0600

    ejercicios print

---
+++ prints/prints.py
@@ -0,0 +1,19 @@
+print ("Hola Mundo")
+print (1245)
+print ("python significa 'serpiente' en inglés pero no es un demonio")
+print(" Men encanta estudiar python")
+print('Estudiar con "Python Total" es super divertido') #se puede usar comilla simple o doble pero para citar debe ser una comilla diferente a la que se usa para escribir el texto
+print(554+1)
+print("Necesito")
+print() #esto da un salto de linea en blanco
+print("Espacio")
+print("Suma:", 10, "+", 5, "=", 15)
+print("Es el año", 2026) # siempre se pone coma para separar los elementos a imprimir, si se pone un signo de suma se concatenan como texto y no se suman como números
+print(""" Este es un texto con varias líneas
+que se puede escribir sin necesidad de usar caracteres especiales
+para saltos de línea
+      kkp.""") #se pueden usar tres compillas para escribir un texto con varias líneas sin necesidad de usar caracteres especiales para saltos de líneas
+print("un string es una cadena de texto")
+print("Esto" + " es" + " una" + " concatenación") #con el signo de suma se concatenan los textos, no se suman como números
+print("/t" + "esto es un tabulador") #el tabulador se representa con \t y da un espacio en blanco más grande que el espacio normal
+print("Esto es una nueva línea\nEsto es otra línea") #el salto de línea se representa con \n y hace que el texto siguiente se imprima en una nueva línea
