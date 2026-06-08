ca2f96850ed35f4616c85bdb9bc892ff4e1c62b2
Diegoudg94 <j.covarrubias@udgvirtual.udg.mx>
2026-06-07 22:03:28 -0600

    ejercicios de input

---
+++ input/input.py
@@ -0,0 +1,7 @@
+# input palablra clave input "se pide el dato al usuario"
+#ejemplo de input
+print(input("¿Cuál es tu nombre?")) #se muestra el mensaje y se espera a que el usuario escriba algo y presione enter, lo que escriba se mostrará en pantalla
+print(input("¿Dime tu apellido?")) #se muestra el mensaje y se espera a que el usuario escriba algo y presione enter, lo que escriba se mostrará en pantalla
+
+print  ("tu nombre es " + input("Cual es tu nombre?")) #se muestra el mensaje y se espera a que el usuario escriba algo y presione enter, lo que escriba se mostrará en pantalla junto con el mensaje "tu nombre es "
+print  ("tu nombre es " + input("Cual es tu nombre?"), "y tu apellido es " + input("Cual es tu apellido?")) #se muestra el mensaje y se espera a que el usuario escriba algo y presione enter, lo que escriba se mostrará en pantalla junto con el mensaje "tu nombre es " y "y tu apellido es "
